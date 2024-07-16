8b249de2bbdc5d1f6dc8b0ee540349f45f2a997e
Is-han-Solo <forishansinha@outlook.com>
2024-07-15 23:32:07 -0400

    added a dark mode to site using themecontext

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { mockHistoricalData } from "../constants/mock";
 import { convertUnixTimestampToDate } from "../helpers/date-helper";
 import Card from "./Card";
 import { Area, AreaChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
 import { chart_config } from "../constants/config";
 import ChartFilter from "./ChartFilter";
+import ThemeContext from "../context/ThemeContext";
 
 const Chart = () => {
+
+    const {darkMode} = useContext(ThemeContext);
 
     const [data, setData] = useState(mockHistoricalData);
     const [filter, setFilter] = useState("1W");
@@ -39,8 +42,8 @@ const Chart = () => {
 
                     <defs>
                         <linearGradient id="chartColor" x1="0" y1="0" x2="0" y2="1">
-                            <stop offset="5%" stopColor="rgb(199 210 254)" stopOpacity={0.8}/>
-                            <stop offset="95%" stopColor="rgb(199 210 254)" stopOpacity={0}/>
+                            <stop offset="5%" stopColor={darkMode ? "#312e81" : "rgb(199 210 254)"} stopOpacity={0.8}/>
+                            <stop offset="95%" stopColor={darkMode ? "#312e81" : "rgb(199 210 254)"} stopOpacity={0}/>
                         </linearGradient>
                     </defs>
                     
@@ -54,7 +57,9 @@ const Chart = () => {
                         fill="url(#chartColor)"
                     />
 
-                    <Tooltip />
+                    <Tooltip 
+                        contentStyle={darkMode ? {backgroundColor: "#111827"} : null}
+                        itemStyle={darkMode ? {color: "#818cf8"} : null}/>
                     <XAxis dataKey={"date"}/>
                     <YAxis domain={["dataMin", "dataMax"]}/>
                 </AreaChart>
